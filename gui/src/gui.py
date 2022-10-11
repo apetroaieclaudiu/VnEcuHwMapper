be8c46bd5cc06059f19db73b07ad3fb62b38bd4b
Claudiu Apetroaie <apetroaie.claudiu@gmail.com>
2022-10-11 21:46:49 +0000

    Started implementing the "functionalities" behind the gui

--- gui/src/gui.py
+++ gui/src/gui.py
@@ -1,8 +1,31 @@
 import sys
+import serial
 from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QComboBox
 from PyQt5.QtGui import QIcon,QPixmap
 
 canch_list = ["CAN1", "CAN2", "CAN3", "CAN4", "CAN5", "CAN6", "CAN7", "CAN8"]
+vnch_list = ["Ch1", "Ch2", "Ch3", "Ch4"]
+vncan_mapping = []
+
+class Raspberry():
+    def __init__(self):
+        self.serial = serial.Serial('/dev/ttyACM0', 115200, timeout=timeout)
+        self.readChannelStatus()
+
+    def send(self, text: str):
+        line = '%s\r\f' % text
+        self.serial.write(line.encode('utf-8'))
+
+    def receive(self) -> str:
+        line = self.serial.read_until(self.TERMINATOR)
+        return line.decode('UTF8').strip()
+
+    def close(self):
+        self.serial.close()
+
+    def readChannelStatus(self):
+        for i in range(1, len(vnch_list)):
+            global vncan_mapping[i] = self.receive()
 
 class App(QWidget):
     def __init__(self):
@@ -42,9 +65,14 @@ class App(QWidget):
         self.chn4_label.setGeometry(200, 200, 120, 30)
         self.setWindowTitle(self.title)
         self.setGeometry(self.left,self.top,self.width,self.height)
+        self.chn1.setCurrentIndex(canch_list.index(vncan_mapping[0]))
+        self.chn2.setCurrentIndex(canch_list.index(vncan_mapping[1]))
+        self.chn3.setCurrentIndex(canch_list.index(vncan_mapping[2]))
+        self.chn4.setCurrentIndex(canch_list.index(vncan_mapping[3]))
         self.show()
 
 if __name__=='__main__':
     app=QApplication(sys.argv)
     ex=App()
+    firmware = Raspberry()
     sys.exit(app.exec())
